5325fca3c7c6b7440e3af6194c829481223008f3
Malandain Timothe <timothe.malandain@epita.fr>
2018-04-20 15:58:37 -0700

    fixes

--- PROJET BAC/clientThr.py
+++ PROJET BAC/clientThr.py
@@ -1,33 +1,19 @@
 
-import base64
 import socket
 import threading
 import sys
 import hashlib
-import os
-import M2Crypto
 from Crypto.Cipher import AES
-import hashlib
 
 
 class Client:
-
-    M2Crypto.Rand.rand_seed(os.urandom(1024))
-    key = M2Crypto.RSA.gen_key(1024, 65537)
-    key.save_key('private.pem', None)
-    key.save_key('public.pem', None)
-
-    print("done -- app started")
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def sendMsg(self):
-
         while True:
-            data = input()
-            #data = Crypto.rsaEncrypt(self, data)
-            self.sock.send(bytes(pseudo + " :" + data, 'utf-8'))
+
+            self.sock.send(bytes(pseudo + " :" + input(), 'utf-8'))
 
     def __init__(self, address):
         self.sock.connect((address, 10000))
@@ -50,5 +36,4 @@ typeVar = input()
 if(len(typeVar) > 1):
     print("client starting on " + typeVar)
     pseudo = input("enter pseudo : ")
-    print("Generating a 1024 bit private/public key pair for " + pseudo + " ...")
     client = Client(typeVar)
--- PROJET BAC/serverThr.py
+++ PROJET BAC/serverThr.py
@@ -3,12 +3,14 @@ import socket
 import threading
 import sys
 
+
 class Server:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     connections = []
+
     def __init__(self):
-        self.sock.bind(('0.0.0.0',10000))
+        self.sock.bind(('0.0.0.0', 10000))
         self.sock.listen(1)
 
     def handler(self, c, a):
@@ -17,23 +19,24 @@ class Server:
             for connections in self.connections:
                 print(a)
                 print(data)
-                connections.send(data)
+                if connections != c:
+                    connections.send(data)
             if not data:
-                print(str(a[0])+':'+str(a[1]),"disconnected")
+                print(str(a[0]) + ':' + str(a[1]), "disconnected")
                 self.connections.remove(c)
                 c.close()
                 break
 
     def run(self):
         while True:
-            c,a = self.sock.accept()
-            cThread = threading.Thread(target=self.handler, args=(c,a))
-            cThread.daemon=True
+            c, a = self.sock.accept()
+            cThread = threading.Thread(target=self.handler, args=(c, a))
+            cThread.daemon = True
             cThread.start()
             self.connections.append(c)
-            print(str(a[0])+':'+str(a[1]),"connected")
+            print(str(a[0]) + ':' + str(a[1]), "connected")
 
 
 print("serveur starting")
-server=Server()
+server = Server()
 server.run()
